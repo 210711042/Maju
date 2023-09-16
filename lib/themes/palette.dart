1498817c5645a82844914022ccb1334edb135289
abrahamjonathanh <abrahamjonathan.acc@gmail.com>
2023-09-16 14:36:09 +0700

    feat(assets): add assets folder feat(lib): add library folder

---
+++ lib/themes/palette.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  //normal
+  static const Color n0 = Color(0xFFFFFFFF);
+  static const Color n50 = Color(0xFFF8F9FD);
+  static const Color n100 = Color(0xFFF4F7FE);
+  static const Color n200 = Color(0xFFD8DCE8);
+  static const Color n300 = Color(0xFFC0C3CF);
+  static const Color n400 = Color(0xFFAEB2BE);
+  static const Color n500 = Color(0xFF8C909E);
+  static const Color n600 = Color(0xFF71747D);
+  static const Color n700 = Color(0xFF4D4F56);
+  static const Color n800 = Color(0xFF303135);
+  static const Color n900 = Color(0xFF18191B);
+
+  //Blue
+  static const Color b100 = Color(0xFFE7F2FF);
+  static const Color b200 = Color(0xFFA3CFFF);
+  static const Color b300 = Color(0xFF5BAAFF);
+  static const Color b400 = Color(0xFF007BFF);
+  static const Color b500 = Color(0xFF0064D2);
+  static const Color b600 = Color(0xFF004FA7);
+  static const Color b700 = Color(0xFF003D81);
+  static const Color b800 = Color(0xFF002A58);
+  static const Color b900 = Color(0xFF001833);
+
+  //Green
+  static const Color g100 = Color(0xFFE2FBED);
+  static const Color g200 = Color(0xFFA8E5C4);
+  static const Color g300 = Color(0xFF66CC94);
+  static const Color g400 = Color(0xFF3CCC7D);
+  static const Color g500 = Color(0xFF0BAE54);
+  static const Color g600 = Color(0xFF088942);
+  static const Color g700 = Color(0xFF076A33);
+  static const Color g800 = Color(0xFF054A24);
+  static const Color g900 = Color(0xFF022612);
+
+  //Red
+  static const Color r400 = Color(0xFFF15C59);
+
+  //Orange
+  static const Color o100 = Color(0xFFFFE9D6);
+  static const Color o200 = Color(0xFFFFC799);
+  static const Color o300 = Color(0xFFFFA55C);
+  static const Color o400 = Color(0xFFFF841F);
+  static const Color o500 = Color(0xFFFF7200);
+  static const Color o600 = Color(0xFFE06500);
+  static const Color o700 = Color(0xFFB85300);
+  static const Color o800 = Color(0xFFB85300);
+  static const Color o900 = Color(0xFF662E00);
+}
